f750ad7826e287cf49bdf508fa5421fc7c2929ab
phornchai Hompromma <76524112+Phorncha@users.noreply.github.com>
2022-09-30 22:58:59 +0700

    updateweb_test

--- web_test/app/Http/Controllers/ProfileController.php
+++ web_test/app/Http/Controllers/ProfileController.php
@@ -15,6 +15,37 @@ class ProfileController extends Controller
         $profile->phone = $req->phone;
         $profile->save();
 
-        return view('postprofile');
+        return redirect("profile_list");
+        
+
+        //return view('postprofile');
+    }
+
+    public function getProfile(){
+        $profile_all = Profile::all();
+        //dd($profile_all);
+        return view('profile_list',['profile'=>$profile_all]);
+    }
+
+    public function delProfile(Request $req){
+
+        Profile::where('id',$req->id)
+        ->delete();
+        return "ok";
+    }
+
+    public function formEdit($id){
+        $profile = Profile::where('id',$id)->first();
+
+        return view('editProfile',['profile'=>$profile]);
+    }
+    
+    public function updateProfile(Request $req){
+        profile::where('id',$req->id)
+        ->update(['first_name'=>$req->first_name,
+                   'last_name'=>$req->last_name,
+                   'email'=>$req->email,
+                   'phone'=>$req->phone,]);
+        return redirect('profile_list');
     }
 }
